1182921249b0bd445eb87253b80533261e032994
nogw <gabrielnogueiraoliveira@gmail.com>
2022-02-03 19:17:46 -0300

    navigate using keys

--- lib/slides_web/live/index_live.ex
+++ lib/slides_web/live/index_live.ex
@@ -31,4 +31,21 @@ defmodule SlidesWeb.IndexLive do
   def handle_event("toggle_menu", _, socket) do
     {:noreply, assign(socket, isOpen: !socket.assigns.isOpen)}
   end
+
+  def handle_event("key_change_slide", %{"key" => "ArrowUp"}, socket) do
+    slideS = socket.assigns.slide
+    slideN = if slideS >= 2, do: slideS - 1, else: length(socket.assigns.pages)
+    IO.inspect(slideS)
+    {:noreply, assign(socket, content: getContent(slideN), slide: slideN)}
+  end
+
+  def handle_event("key_change_slide", %{"key" => "ArrowDown"}, socket) do
+    slideS = socket.assigns.slide
+    slideN = if slideS == length(socket.assigns.pages), do: 1, else: slideS + 1
+    {:noreply, assign(socket, content: getContent(slideN), slide: slideN)}
+  end
+
+  def handle_event("key_change_slide", _key, socket) do
+    {:noreply, socket}
+  end
 end
